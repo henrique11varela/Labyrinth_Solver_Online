e87a43577cb63c38fe2438fd19ac8605fea198a2
henrique11varela <henrique11varela@gmail.com>
2023-02-10 12:12:11 +0000

    added default example labyrinth

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 //Global var
-let height = 0;
-let width = 0;
-let mat = [];
+let height = 16;
+let width = 16;
 let currentTool = 0; //0-toggle 1-start 2-end
+let mat = [[1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1],
+[1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+[1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+[1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+[1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+[1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1],
+[1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+[1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+[1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1],
+[1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+[1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+[1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1]];
 
 function readBoardSize() {
     height = document.getElementById("Height").value;
     width = document.getElementById("Width").value;
+    resetBoard();
     buildBoard();
+}
+
+function resetBoard() {
+    for (let h = 0; h < height; h++) {
+        mat[h] = [];
+        for (let w = 0; w < width; w++) {
+            mat[h][w] = (h == 0 || w == 0 || h == height - 1 || w == width - 1) ? 1 : 0;
+        }
+    }
 }
 
 function buildBoard() {
     // Build board
     let board = "";
     for (let h = 0; h < height; h++) {
-        mat[h] = [];
         for (let w = 0; w < width; w++) {
-            mat[h][w] = (h == 0 || w == 0 || h == height - 1 || w == width - 1) ? 1 : 0;
-            board += "<div id=\"" + h + "_" + w + "\" class=\"pixel " + (mat[h][w] == 1 ? "wall" : "") + "\" style=\"height: " + (100 / height) + "%;width: " + (100 / width) + "%;\" onclick=\"pixel(" + h + ", " + w + ")\"></div>"; // insert "pixel"
+            board += "<div id=\"" + h + "_" + w + "\" class=\"pixel" + (mat[h][w] == 1 ? " wall" : mat[h][w] == 2 ? " point" : "") + "\" style=\"height: " + (100 / height) + "%;width: " + (100 / width) + "%;\" onclick=\"pixel(" + h + ", " + w + ")\"></div>"; // insert "pixel"
         }
     }
     // Inject board
@@ -41,19 +64,29 @@ function pixel(h, w) {
         }
     }
     else if (currentTool == 1) {
-        mat[h][w] = 2;
-        pixelElement.classList.remove("wall");
-        pixelElement.classList.remove("end");
-        pixelElement.classList.add("start");
+        if (counter(2) < 2) {
+            mat[h][w] = 2;
+            pixelElement.classList.remove("wall");
+            pixelElement.classList.remove("end");
+            pixelElement.classList.add("start");
+        }
+        else {
+            alert("There's already 2 endpoints");
+        }
         currentTool = 0;
     }
-    else if (currentTool == 2) {
-        mat[h][w] = 3;
-        pixelElement.classList.remove("wall");
-        pixelElement.classList.remove("start");
-        pixelElement.classList.add("end");
-        currentTool = 0;
+}
+
+function counter(value) {
+    let c = 0;
+    for (let h = 0; h < height; h++) {
+        for (let w = 0; w < width; w++) {
+            if (mat[h][w] == value) {
+                c++;
+            }
+        }
     }
+    return c;
 }
 
 function changeTool(tool) {
@@ -65,7 +98,7 @@ function solve() {
 }
 
 //main---------------------------------------------------------------------------
-readBoardSize();
+buildBoard();
 
 
 
